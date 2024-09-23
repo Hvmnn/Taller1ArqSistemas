4e84de0884cd1da2dbfe45540adf5bb23502814c
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-09-23 19:58:27 -0300

    fix: add new variables to grades model

--- Models/Grades.cs
+++ Models/Grades.cs
@@ -1,8 +1,16 @@
-public class Grades
-{
+namespace GradesService.Models{
+    public class Grades
+    {
+        public Grades()
+        {
+            Id = Guid.NewGuid().ToString(); 
+        }
+
+    public string Id { get; set; }
+    public string StudentId { get; set; }
     public string subjectName { get; set; } = string.Empty;
     public string gradeName { get; set; } = string.Empty;
     public int grade { get; set; }
     public string comment { get; set; } = string.Empty;
-    
+    }
 }
