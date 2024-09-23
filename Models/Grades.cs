f2e8f478fc0300e663d13955813c611a3ad6fecc
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-09-23 17:36:18 -0300

    feat: add grades model

---
+++ Models/Grades.cs
@@ -0,0 +1,8 @@
+public class Grades
+{
+    public string subjectName { get; set; } = string.Empty;
+    public string gradeName { get; set; } = string.Empty;
+    public int grade { get; set; }
+    public string comment { get; set; } = string.Empty;
+    
+}
